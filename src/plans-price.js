5d0600c7cf87a51383deb462d62bcf10a7c0d040
Lautaro <espinillo.lautaro@gmail.com>
2024-11-19 10:24:54 -0300

    updated

--- src/plans-price.js
+++ src/plans-price.js
@@ -1,8 +1,10 @@
 const $precio_mensual_1 = document.querySelector("#precio_mensual_1")
 const $precio_mensual_2 = document.querySelector("#precio_mensual_2")
+const $precio_mensual_3 = document.querySelector("#precio_mensual_3")
 const $precio_mensual_vip = document.querySelector("#precio_mensual_vip")
 const $precio_trimestral_1 = document.querySelector("#precio_trimestral_1")
 const $precio_trimestral_2 = document.querySelector("#precio_trimestral_2")
+const $precio_trimestral_3 = document.querySelector("#precio_trimestral_3")
 const $precio_trimestral_vip = document.querySelector("#precio_trimestral_vip")
 const $cupos_plan_vip = document.querySelector("#cupos_plan_vip")
 
@@ -43,31 +45,36 @@ let prices = fetch("https://sheetdb.io/api/v1/jlbgbujbxmu56")
   .then((data) => cargarPrecios(data, "arg"))
 
 function cargarPrecios(data, country) {
+  console.log(data)
   if (country === "arg") {
     // Precios mensuales
     $precio_mensual_1.textContent = data[0].PRECIO_MENSUAL
     $precio_mensual_2.textContent = data[1].PRECIO_MENSUAL
-    $precio_mensual_vip.textContent = data[2].PRECIO_MENSUAL
+    $precio_mensual_3.textContent = data[2].PRECIO_MENSUAL
+    $precio_mensual_vip.textContent = data[3].PRECIO_MENSUAL
 
     // Precios trimestrales
     $precio_trimestral_1.textContent = data[0].PRECIO_TRIMESTRAL
     $precio_trimestral_2.textContent = data[1].PRECIO_TRIMESTRAL
-    $precio_trimestral_vip.textContent = data[2].PRECIO_TRIMESTRAL
+    $precio_trimestral_3.textContent = data[2].PRECIO_TRIMESTRAL
+    $precio_trimestral_vip.textContent = data[3].PRECIO_TRIMESTRAL
 
     // Cupos
-    $cupos_plan_vip.textContent = data[2].CUPOS
+    $cupos_plan_vip.textContent = data[3].CUPOS
   } else {
     // Precios mensuales
     $precio_mensual_1.textContent = data[0].PRECIO_MENSUAL_USD
     $precio_mensual_2.textContent = data[1].PRECIO_MENSUAL_USD
-    $precio_mensual_vip.textContent = data[2].PRECIO_MENSUAL_USD
+    $precio_mensual_3.textContent = data[2].PRECIO_MENSUAL_USD
+    $precio_mensual_vip.textContent = data[3].PRECIO_MENSUAL_USD
 
     // Precios trimestrales
     $precio_trimestral_1.textContent = data[0].PRECIO_TRIMESTRAL_USD
     $precio_trimestral_2.textContent = data[1].PRECIO_TRIMESTRAL_USD
-    $precio_trimestral_vip.textContent = data[2].PRECIO_TRIMESTRAL_USD
+    $precio_trimestral_3.textContent = data[2].PRECIO_TRIMESTRAL_USD
+    $precio_trimestral_vip.textContent = data[3].PRECIO_TRIMESTRAL_USD
 
     // Cupos
-    $cupos_plan_vip.textContent = data[2].CUPOS
+    $cupos_plan_vip.textContent = data[3].CUPOS
   }
 }
